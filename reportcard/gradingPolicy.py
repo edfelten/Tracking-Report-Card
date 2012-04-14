db84b86e7df355a0ac7128dbdcd703b95771608b
Ed Felten <felten@cs.princeton.edu>
2012-04-14 11:48:40 -0400

    first version of reportcard files

---
+++ reportcard/gradingPolicy.py
@@ -0,0 +1,15 @@
+
+rubric = {
+  'usesTrackingCookie' : -1,
+  'hasOptOut' : 1,
+  'dropsTrackingCookieOnOptOut' : 1,
+  'evil' : -5
+}
+
+curve = (
+  (40, 'A'),
+  (30, 'B'),
+  (20, 'C'),
+  (10, 'D'),
+)
+
